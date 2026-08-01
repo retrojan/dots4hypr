8b4104cf334c0b5f35ecb885ba22132550f800cf
retrojan <175008938+retrojan@users.noreply.github.com>
2026-08-01 16:14:30 +0000

    Delete hypr directory

--- hypr/modules/monitors.lua
+++
@@ -1,6 +0,0 @@
-hl.monitor({
-    output = "",
-    mode = "2560x1440@165",
-    position = "0x0",
-    scale = "1",
-})
--- hypr/scripts/screenshot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wayfreeze & PID=$!
-sleep 0.1
-grim -g "$(slurp -b '#00000099')" - | wl-copy
-kill $PID
