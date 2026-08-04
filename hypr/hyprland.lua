66e534fdd5ce8917c7be3154db4ed8c4b25e1a22
retrojan <175008938+retrojan@users.noreply.github.com>
2026-08-04 09:48:07 +0200

    Update hyprland.lua

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -7,15 +7,19 @@
 --                 https://github.com/retrojan/dots-hypr
 --
 
-
+-- display config
 require("modules.monitors")
-
+-- envs
 require("modules.env")
+-- autostart
 require("modules.autostart")
-
+-- settings
 require("modules.settings")
+-- layout, devices
 require("modules.input")
+-- binds
 require("modules.binds")
-
+-- rules
 require("modules.rules")
+-- misc
 require("modules.misc")
